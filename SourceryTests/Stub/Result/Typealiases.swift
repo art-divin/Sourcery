3e4fcbc137bb3ee7855cb94d9acdb16639b95308
David Jennes <david.jennes@gmail.com>
2023-10-07 18:30:42 +0200

    EJSTemplates: make `typealias`es accessible from templates

---
+++ SourceryTests/Stub/Result/Typealiases.swift
@@ -0,0 +1,7 @@
+// Generated using Sourcery Major.Minor.Patch — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+// Typealiases in Bar
+// - name 'List', type '[Foo]'
+// Typealiases in Foo
+// - name 'Bar', type '[String: String]'
+// Typealiases in FooSubclass
--- SourceryTests/Stub/Source/Foo.swift
+++ SourceryTests/Stub/Source/Foo.swift
@@ -1,6 +1,7 @@
 import Foundation
 
 class Foo {
+    typealias Bar = [String: String]
     var name: String = ""
     var value: Int = 0
 }
